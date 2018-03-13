90acef2136c4e5255e7a6dbbef92f13974643887
Nicholas Carl Roberts <nick11roberts@users.noreply.github.com>
2018-03-13 00:14:23 -0700

    Added dataset url

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'click',
+        'emoji',
+        'numpy',
     ],
     entry_points='''
         [console_scripts]
--- tcsubmit.py
+++ tcsubmit.py
@@ -1,8 +1,11 @@
 import click
+import emoji
+import numpy as np
 
 @click.command()
-@click.argument('git_url', nargs=-1)
-def cli(git_url):
+@click.argument('git_url')
+@click.argument('dataset_url')
+def cli(git_url, dataset_url):
     """Script used to submit PyTorch models to the blockchain for training.
 
     \b
@@ -10,5 +13,37 @@ def cli(git_url):
         tcsubmit https://github.com/nick11roberts/PGCnet/
     """
 
-    ### Sends request containing url to someone in the mining pool
-    click.echo(git_url)
+    click.echo("Validating repository against submission guidelines...")
+    ### TODO Download repo, check repo structure, run tests, etc...
+
+    # For now, randomly select pass/fail
+    #if np.random.choice(2):
+    if False:
+        click.echo(
+            """
+            Repository does not meet specifications, see
+            https://github.com/nick11roberts/torch-coin
+            for more information and formatting instructions.
+            """)
+        exit()
+
+    click.echo(
+        emoji.emojize(
+            "Passed! :smile: :100: :thumbs_up:", use_aliases=True))
+
+    ### TODO Send model to someone in the mining pool
+
+    click.echo(
+        emoji.emojize(
+            ":zap: Submitting model...",
+            use_aliases=True))
+
+    click.echo(
+        emoji.emojize(
+            ":mushroom: Model submitted.",
+            use_aliases=True))
+
+    click.echo(
+        emoji.emojize(
+            ":mailbox_with_mail: Download instructions for your trained model will be sent via email, hang tight.",
+            use_aliases=True))
